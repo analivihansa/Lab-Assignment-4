914440d401f2af2ecac95abff044390b42604128
Anali Fernando <fernandopav.21@uom.lk>
2024-05-20 16:49:34 +0530

    inventory class added to project

---
+++ inventory.py
@@ -0,0 +1,15 @@
+from product import Product
+
+
+class Inventory:
+
+    def __init__(self):
+        self.products = []
+
+    def add_product(self, product):
+        self.products.append(product)
+
+    def list_products(self):
+        return self.products
+
+    
